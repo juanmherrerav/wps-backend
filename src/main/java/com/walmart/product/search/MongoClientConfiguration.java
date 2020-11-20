d500394bed37d5017860da20e4acd0471de1a918
Juan Herrera <juan.herrera@sovos.com>
2020-11-20 01:37:43 -0300

    Springboot build on image

--- src/main/java/com/walmart/product/search/MongoClientConfiguration.java
+++ src/main/java/com/walmart/product/search/MongoClientConfiguration.java
@@ -4,16 +4,20 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoClientConfiguration   {
+    @Autowired
+    private Environment env;
 
     @Bean
     public MongoClient mongo() throws Exception {
-        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/admin");
+        final ConnectionString connectionString = new ConnectionString(env.getProperty("spring.data.mongodb.uri"));
         final MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
         return MongoClients.create(mongoClientSettings);
     }
