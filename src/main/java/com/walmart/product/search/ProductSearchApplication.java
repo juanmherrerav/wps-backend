332546893ce9203cfb2ba21ed05fe775d2561687
Juan Herrera <juan.herrera@sovos.com>
2020-11-20 06:37:45 -0300

    mongo conf spring boot

--- src/main/java/com/walmart/product/search/ProductSearchApplication.java
+++ src/main/java/com/walmart/product/search/ProductSearchApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductSearchApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProductSearchApplication.class, args);
 	}
